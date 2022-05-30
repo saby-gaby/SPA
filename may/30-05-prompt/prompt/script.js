0c8d17e3b1df48e0bd817ff2094d4f3dbd8a9f1d
sabina <sgudzenova@gmail.com>
2022-05-30 10:20:44 +0200

    initial commit

---
+++ may/30-05-prompt/prompt/script.js
@@ -0,0 +1,14 @@
+// console.log("Hello World!");   --> checken if is working in browser  -> F12 & console
+
+console.log(`start`);
+
+// Frage an Nutzer
+const userName = prompt("Wie heißt du?");    // pause
+
+console.log(`test`);
+
+console.log(`Willkommen auf meine Seite, ${userName}!`);  
+
+const favFood = prompt("Welches ist dein Lieblingsessen?");    // pause
+
+console.log(`${userName} liebt ${favFood}.`);
