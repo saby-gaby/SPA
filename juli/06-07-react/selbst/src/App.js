b43f2c880742657584c4649b315d63c96b2f297f
sabina <sgudzenova@gmail.com>
2022-07-06 11:52:15 +0200

    react-start

---
+++ juli/06-07-react/selbst/src/App.js
@@ -0,0 +1,13 @@
+import logo from './birdy.png'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <img src={logo} className="logo" alt="logo" />
+            <h1 className="twitter-blue">Zwitscher</h1>
+        </div>
+    )
+}
+
+export default App
