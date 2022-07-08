3cda7aa217223994694346ad71c73a8513319358
sabina <sgudzenova@gmail.com>
2022-07-08 16:25:45 +0200

    css correction

--- mentoring/08-07-react/src/Carousel.js
+++ mentoring/08-07-react/src/Carousel.js
@@ -13,7 +13,11 @@ export default function () {
           <img
             src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg"
             className="d-block w-100"
-            style={{ height: "408px", objectFit: "cover" }}
+            style={{
+              height: "408px",
+              objectFit: "cover",
+              objectPosition: "center 30%",
+            }}
             alt="..."
           />
         </div>
@@ -21,6 +25,11 @@ export default function () {
           <img
             src="https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg"
             className="d-block w-100"
+            style={{
+              height: "408px",
+              objectFit: "cover",
+              objectPosition: "center 30%",
+            }}
             alt="..."
           />
         </div>
@@ -28,7 +37,11 @@ export default function () {
           <img
             src="https://th-thumbnailer.cdn-si-edu.com/SdKYWifCKfE2g8O-po_SO99hQ-Y=/1000x750/filters:no_upscale():focal(3126x2084:3127x2085)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/ec/e6/ece69181-708a-496e-b2b7-eaf7078b99e0/gettyimages-1310156391.jpg"
             className="d-block w-100"
-            style={{ height: "408px", objectFit: "cover" }}
+            style={{
+              height: "408px",
+              objectFit: "cover",
+              objectPosition: "center 30%",
+            }}
             alt="..."
           />
         </div>
